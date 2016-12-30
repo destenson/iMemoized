3ce26c8f0893e97d423cd88c73c50548e1abdc18
Simon Blackwell <syblackwell@anywhichway.com>
2016-12-30 07:26:53 -0500

    removed pretest from benchmark

--- jsbenchmark/benchmark.js
+++ jsbenchmark/benchmark.js
@@ -1,11 +1,4 @@
 (function() {
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-45402411-4', 'auto');
-  ga('send', 'pageview');
   
 	var imports = ["https://cdnjs.cloudflare.com/ajax/libs/memoizejs/0.1.1/memoize.min.js","fast-memoize.js","moize.js","aomemoize.js","iMemoized.js"];
 
@@ -49,7 +42,7 @@
 				singleMemoizeFibonacci: {
 					arguments: [num],
 					expected: fibonacci(num),
-					pretest: true,
+					pretest: false,
 					showArguments: true,
 					tests: {
 						vanillaFibonacci: fibonacci,
@@ -64,7 +57,7 @@
 				multiMemoizeFibonacci: {
 					arguments: [num],
 					expected: fibonacci(num),
-					pretest: true,
+					pretest: false,
 					showArguments: true,
 					tests: {
 						vanilla: fibonacciMultiplePrimitive,
@@ -78,7 +71,7 @@
 				multiObjectMemoizeFibonacci: {
 					arguments: [num,{isComplete:false}],
 					expected: fibonacci(num),
-					pretest: true,
+					pretest: false,
 					showArguments: true,
 					tests: {
 						vanilla: fibonacciMultipleObject,
